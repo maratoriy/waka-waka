8c5b340e4949763253c6646df939eb063cbedadf
moratorium <spgamermdk@gmail.com>
2021-03-07 20:33:34 +0300

    1.0.1

---
+++ release/release/qmlcache_loader.cpp
@@ -0,0 +1,316 @@
+#include <QtQml/qqmlprivate.h>
+#include <QtCore/qdir.h>
+#include <QtCore/qurl.h>
+
+namespace QmlCacheGeneratedCode {
+namespace _pages_components_tasks_MultipleVariants_task_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_TextAnswer_result_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_InfoComponent_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_TextAnswer_create_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_TextAnswer_task_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__backend_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_TextAnswer_update_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__about_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_TestPage_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__data_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_ResultsPage_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__create_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_UpdatePage_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_MultipleVariants_update_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_TableAnswer_task_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_TaskComponent_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_FixedNumber_task_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_FixedNumber_result_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__main_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_FixedNumber_create_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_TableAnswer_result_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_TableAnswer_create_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__open_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_CreatePage_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_controls_MyTextField_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__home_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_FourVariants_result_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_FourVariants_task_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_FourVariants_create_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_CreateTaskComponent_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_taskComponents_QuestionContent_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_SettingsPage_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_MultipleVariants_result_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_MultipleVariants_create_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_UpdateTaskComponent_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_FixedNumber_update_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_ResultsTaskComponent_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _pages_components_tasks_TableAnswer_update_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+
+}
+namespace {
+struct Registry {
+    Registry();
+    ~Registry();
+    QHash<QString, const QQmlPrivate::CachedQmlUnit*> resourcePathToCachedUnit;
+    static const QQmlPrivate::CachedQmlUnit *lookupCachedUnit(const QUrl &url);
+};
+
+Q_GLOBAL_STATIC(Registry, unitRegistry)
+
+
+Registry::Registry() {
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/MultipleVariants/task.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_MultipleVariants_task_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/TextAnswer/result.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_TextAnswer_result_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/InfoComponent.qml"), &QmlCacheGeneratedCode::_pages_components_InfoComponent_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/TextAnswer/create.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_TextAnswer_create_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/TextAnswer/task.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_TextAnswer_task_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/backend.qml"), &QmlCacheGeneratedCode::_0x5f__backend_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/TextAnswer/update.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_TextAnswer_update_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/about.qml"), &QmlCacheGeneratedCode::_0x5f__about_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/TestPage.qml"), &QmlCacheGeneratedCode::_pages_TestPage_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/data.qml"), &QmlCacheGeneratedCode::_0x5f__data_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/ResultsPage.qml"), &QmlCacheGeneratedCode::_pages_ResultsPage_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/create.qml"), &QmlCacheGeneratedCode::_0x5f__create_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/UpdatePage.qml"), &QmlCacheGeneratedCode::_pages_UpdatePage_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/MultipleVariants/update.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_MultipleVariants_update_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/TableAnswer/task.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_TableAnswer_task_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/TaskComponent.qml"), &QmlCacheGeneratedCode::_pages_components_TaskComponent_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/FixedNumber/task.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_FixedNumber_task_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/FixedNumber/result.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_FixedNumber_result_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/main.qml"), &QmlCacheGeneratedCode::_0x5f__main_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/FixedNumber/create.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_FixedNumber_create_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/TableAnswer/result.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_TableAnswer_result_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/TableAnswer/create.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_TableAnswer_create_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/open.qml"), &QmlCacheGeneratedCode::_0x5f__open_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/CreatePage.qml"), &QmlCacheGeneratedCode::_pages_CreatePage_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/controls/MyTextField.qml"), &QmlCacheGeneratedCode::_pages_components_controls_MyTextField_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/home.qml"), &QmlCacheGeneratedCode::_0x5f__home_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/FourVariants/result.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_FourVariants_result_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/FourVariants/task.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_FourVariants_task_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/FourVariants/create.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_FourVariants_create_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/CreateTaskComponent.qml"), &QmlCacheGeneratedCode::_pages_components_CreateTaskComponent_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/taskComponents/QuestionContent.qml"), &QmlCacheGeneratedCode::_pages_components_taskComponents_QuestionContent_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/SettingsPage.qml"), &QmlCacheGeneratedCode::_pages_SettingsPage_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/MultipleVariants/result.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_MultipleVariants_result_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/MultipleVariants/create.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_MultipleVariants_create_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/UpdateTaskComponent.qml"), &QmlCacheGeneratedCode::_pages_components_UpdateTaskComponent_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/FixedNumber/update.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_FixedNumber_update_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/ResultsTaskComponent.qml"), &QmlCacheGeneratedCode::_pages_components_ResultsTaskComponent_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/pages/components/tasks/TableAnswer/update.qml"), &QmlCacheGeneratedCode::_pages_components_tasks_TableAnswer_update_qml::unit);
+    QQmlPrivate::RegisterQmlUnitCacheHook registration;
+    registration.version = 0;
+    registration.lookupCachedQmlUnit = &lookupCachedUnit;
+    QQmlPrivate::qmlregister(QQmlPrivate::QmlUnitCacheHookRegistration, &registration);
+}
+
+Registry::~Registry() {
+    QQmlPrivate::qmlunregister(QQmlPrivate::QmlUnitCacheHookRegistration, quintptr(&lookupCachedUnit));
+}
+
+const QQmlPrivate::CachedQmlUnit *Registry::lookupCachedUnit(const QUrl &url) {
+    if (url.scheme() != QLatin1String("qrc"))
+        return nullptr;
+    QString resourcePath = QDir::cleanPath(url.path());
+    if (resourcePath.isEmpty())
+        return nullptr;
+    if (!resourcePath.startsWith(QLatin1Char('/')))
+        resourcePath.prepend(QLatin1Char('/'));
+    return unitRegistry()->resourcePathToCachedUnit.value(resourcePath, nullptr);
+}
+}
+int QT_MANGLE_NAMESPACE(qInitResources_qml)() {
+    ::unitRegistry();
+    Q_INIT_RESOURCE(qml_qmlcache);
+    return 1;
+}
+Q_CONSTRUCTOR_FUNCTION(QT_MANGLE_NAMESPACE(qInitResources_qml))
+int QT_MANGLE_NAMESPACE(qCleanupResources_qml)() {
+    Q_CLEANUP_RESOURCE(qml_qmlcache);
+    return 1;
+}
